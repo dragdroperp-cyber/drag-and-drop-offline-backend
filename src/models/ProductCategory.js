9e621826b1a25b0291d6e009c2b3d1e0cb513182
dragdroperp-cyber <dragdroperp@gmail.com>
2025-12-23 12:34:40 +0530

    Add files via upload

---
+++ src/models/ProductCategory.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const ProductCategory = new mongoose.Schema({
+        sellerId:{
+            type:mongoose.Schema.Types.ObjectId,
+            required:true,
+            ref:"Seller"
+        },
+        name:{
+            type:String,
+            required:true
+        },
+        isActive:{
+            type:Boolean,
+            default:true
+        },
+        description:String,
+        isDeleted: {
+            type: Boolean,
+            default: false,
+            index: true
+        }
+}, { timestamps: true });
+
+module.exports = mongoose.model("ProductCategory",ProductCategory);
